4a415b3370d585cc4e080e975ebf0d13207de55e
Vinaya Dahal <vinayadahal@gmail.com>
2021-01-17 23:00:59 +0545

    spring security classes created

---
+++ grails-app/services/websitemanager/UserRoleService.groovy
@@ -0,0 +1,18 @@
+package websitemanager
+
+import grails.gorm.services.Service
+
+@Service(UserRole)
+interface UserRoleService {
+
+    UserRole get(Serializable id)
+
+    List<UserRole> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    UserRole save(UserRole userRole)
+
+}
